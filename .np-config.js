2cb43ce2a51a4dda485749ec983aabb9e99d3496
magicLost <oooze@mail.ru>
2021-08-25 17:55:30 +0300

    Prepare to make pre-compiled versions of components

--- .np-config.js
+++ .np-config.js
@@ -7,7 +7,7 @@ module.exports = {
   cleanup: false,
   yarn: false,
   // Subdirectory to publish
-  contents: ".",
+  contents: "./dist",
   // Open a GitHub release draft after releasing
   releaseDraft: true,
   // Show tasks without actually executing them
--- src/theme/merge.ts
+++ src/theme/merge.ts
@@ -1,5 +1,7 @@
 import { merge as mergeDeep } from "./utils";
-const mainConf = require("./../../tailwind.config");
+// const mainConf = require("./../../tailwind.config");
+const pathToConfig = "./../tailwind.config";
+const mainConf = require(pathToConfig);
 
 export const merge = (config: any) => {
   return mergeConfig(config, mainConf);
